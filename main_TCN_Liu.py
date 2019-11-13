ef55b131aa3a92d8ffc5fca6f797f35e5e2c5c4d
Dewald928 <dewald123@rocketmail.com>
2019-11-13 09:42:07 +0200

    quick paperspace gpu test

--- main_TCN_Liu.py
+++ main_TCN_Liu.py
@@ -695,7 +695,7 @@ if __name__ == '__main__':
 
     # parse hyperparameters
     parser = argparse.ArgumentParser(description='Deep Flare Prediction')
-    parser.add_argument('--epochs', type=int, default=100,
+    parser.add_argument('--epochs', type=int, default=10,
                         help='upper epoch limit (default: 100)')
     parser.add_argument('--flare_label', default="M5",
                         help='Types of flare class (default: M-Class')
@@ -706,9 +706,9 @@ if __name__ == '__main__':
     parser.add_argument('--layer_dim', type=int, default=1, metavar='N',
                         help='how many hidden layers (default: 5)')
 
-    parser.add_argument('--levels', type=int, default=1,
+    parser.add_argument('--levels', type=int, default=2,
                         help='# of levels (default: 4)')
-    parser.add_argument('--ksize', type=int, default=5,
+    parser.add_argument('--ksize', type=int, default=2,
                         help='kernel size (default: 5)')
     parser.add_argument('--nhid', type=int, default=40,
                         help='number of hidden units per layer (default: 128)')
