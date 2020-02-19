8465b7016dda26b75e2c8a2bca64e6c6715f63d2
Dewald928 <dewald123@rocketmail.com>
2020-02-19 10:50:20 +0200

    stopping metric changed too tss

--- Test_Scripts/workers_test.py
+++ Test_Scripts/workers_test.py
@@ -2,28 +2,22 @@ import torch
 from torchvision import datasets, transforms
 import time
 
-
 if __name__ == '__main__':
     use_cuda = torch.cuda.is_available()
 
-    for num_workers in range(0,20,1):
-        kwargs = {'num_workers': num_workers, 'pin_memory': True} if use_cuda \
-            else {}
+    for num_workers in range(0, 20, 1):
+        kwargs = {'num_workers': num_workers,
+                  'pin_memory': True} if use_cuda else {}
         train_loader = torch.utils.data.DataLoader(
             datasets.MNIST('./data', train=True, download=True,
-                           transform=transforms.Compose([
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.1307,), (0.3081,))
-                           ])),
+                           transform=transforms.Compose([transforms.ToTensor(),
+                               transforms.Normalize((0.1307,), (0.3081,))])),
             batch_size=64, shuffle=True, **kwargs)
-
-
 
         start = time.time()
         for epoch in range(1, 5):
             for batch_idx, (data, target) in enumerate(train_loader):
                 pass
         end = time.time()
-        print("Finish with:{} second, num_workers={}".format(end-start,num_workers))
-
-
+        print("Finish with:{} second, num_workers={}".format(end - start,
+                                                             num_workers))
