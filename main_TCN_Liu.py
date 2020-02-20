4ffea5fde9c9c5dc4e8a53c6720a77a9074cfc34
Dewald928 <dewald123@rocketmail.com>
2020-02-20 14:13:57 +0200

    disabled deterministic

--- main_TCN_Liu.py
+++ main_TCN_Liu.py
@@ -334,7 +334,7 @@ if __name__ == '__main__':
     torch.manual_seed(args.seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(args.seed)
-    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.deterministic = False
     torch.backends.cudnn.benchmark = True
     np.random.seed(args.seed)
 
