5520ce88922d7bd8b144fb7d2a3636bb206177b3
Dewald928 <dewald123@rocketmail.com>
2020-05-12 12:06:34 +0200

    nice graphs of std
    Took 7 hours 21 minutes

--- Test_Scripts/wandb_api.py
+++ Test_Scripts/wandb_api.py
@@ -5,24 +5,26 @@ import os.path
 import seaborn as sns
 import matplotlib.pyplot as plt
 api = wandb.Api()
+filename = 'all_runs.csv'
+all_runs = pd.read_csv(filename) if os.path.isfile(
+    filename) is True else pd.DataFrame()
 
-# # Get runs from a specific sweep
+
+# Get runs from a specific sweep
 # sweep0 = api.sweep("dewald123/liu_pytorch_MLP/xris1c8z")
 # sweep1 = api.sweep("dewald123/liu_pytorch_MLP/0iihk32s")
 # sweep2 = api.sweep("dewald123/liu_pytorch_MLP/1c6ig5mc")
-# sweep = api.sweep("dewald123/liu_pytorch_MLP/1c6ig5mc")
+# # sweep = api.sweep("dewald123/liu_pytorch_MLP/1c6ig5mc")
 # sweeps = [sweep0, sweep1, sweep2]
-
-all_runs = pd.read_csv('all_runs.csv') if os.path.isfile(
-    'all_runs.csv') is True else pd.DataFrame()
-
+#
+#
 # for sweep in sweeps:
 #     # if the file is already written to, read it
-#     all_runs = pd.read_csv('all_runs.csv') if os.path.isfile(
-#         'all_runs.csv') is True else pd.DataFrame()
+#     all_runs = pd.read_csv(filename) if os.path.isfile(
+#         filename) is True else pd.DataFrame()
 #
 #     for i in range(len(sweep.runs)):
-#         w = 3
+#         w = 20
 #         run_data = pd.DataFrame()
 #         # wandb.init(project='liu_pytorch_MLP', entity='dewald123',
 #         #            name=sweep.runs[i].name, id=sweep.runs[i].id, resume=True,
@@ -31,6 +33,9 @@ all_runs = pd.read_csv('all_runs.csv') if os.path.isfile(
 #         # get val tss for run
 #         history = sweep.runs[i].history(samples=1000)
 #         val_tss = history["Validation_TSS"].dropna().reset_index(drop=True)
+#         test_tss = history["Test_TSS"].dropna().reset_index(drop=True)
+#         test_tss = test_tss.append([test_tss]*(len(val_tss)-1),
+#                                      ignore_index=True)
 #         run_id = sweep.runs[i].id
 #         run_config = sweep.runs[i].config
 #         run_config = {k: run_config[k] for k in
@@ -40,7 +45,7 @@ all_runs = pd.read_csv('all_runs.csv') if os.path.isfile(
 #         run_config.update({'id': run_id})
 #         run_config = pd.DataFrame(run_config, index=range(1))
 #         run_config = run_config.append([run_config] * (len(val_tss)-1),
-#                                      ignore_index=True)
+#                                        ignore_index=True)
 #
 #         # calculate moving avg and std
 #         moving_avg = val_tss.expanding(min_periods=1).mean()
@@ -62,12 +67,14 @@ all_runs = pd.read_csv('all_runs.csv') if os.path.isfile(
 #         moving_std.name = 'moving_std'
 #         moving_std_w.name = 'moving_std_w'
 #         moving_std_g.name = 'moving_std_g'
+#         test_tss.name = 'Test_TSS'
 #         val_tss = val_tss.to_frame()
 #
 #         val_tss['epoch'] = np.arange(len(val_tss))
 #
 #         # create dataframe from the run
-#         run_data = pd.concat([run_config, val_tss, moving_avg, moving_std,
+#         run_data = pd.concat([run_config, val_tss, test_tss, moving_avg,
+#                               moving_std,
 #                               moving_avg_w, moving_std_w, moving_avg_g, moving_std_g],
 #                              axis=1)
 #
@@ -76,18 +83,33 @@ all_runs = pd.read_csv('all_runs.csv') if os.path.isfile(
 #         print(i)
 #
 #     # dataframe to csv
-#     all_runs.to_csv('all_runs.csv')
+#     all_runs.to_csv(filename)
+#
 #
 
 
 
-
 # seaborn plots
-layers = 1
+layers = 2
 hidden_units = 40
-batch_size = 4096
+batch_size = 8192
 learning_rate = 1e-3
-seed = 15
+seed = 335
+
+# select run values
+run_data = all_runs[(all_runs['layers'] == layers)
+                                 & (all_runs['hidden_units'] == hidden_units)
+                                 & (all_runs['batch_size'] == batch_size)
+                                 & (all_runs['learning_rate'] == learning_rate)
+                                 ]
+# get best validation epoch and test score at that point
+test_tss = run_data.groupby('seed').apply(lambda x: x[
+    'Test_TSS'].unique())
+best_epoch_idx = run_data.groupby('seed').apply(lambda x: x[
+    'Validation_TSS'].idxmax())
+best_epoch = run_data['epoch'][best_epoch_idx]
+std_at_epoch = run_data['moving_std_w'][best_epoch_idx]
+
 
 fig, ax1 = plt.subplots(figsize=(10, 6))
 color = 'tab:blue'
@@ -95,31 +117,33 @@ ax1.set_title('Validation TSS and standard deviations', fontsize=16)
 ax1.set_xlabel('Epoch', fontsize=16)
 ax1.set_ylabel('Validation TSS', fontsize=16, color=color)
 ax1.set_ylim(0, 1)
+# ax3 = sns.scatterplot(best_epoch, test_tss, color='g', ax=ax1)
+plt.scatter(best_epoch.to_numpy(), test_tss.to_numpy(), c='g')
+test_str = ''
+for i in range(len(test_tss)):
+    test_str = test_str + '{}. {} ({})\n'.format(i, test_tss.iloc[i],
+                                         test_tss.index[i])
+plt.legend(['[Test TSS] (seed) \n' + test_str], loc=7)
+# ax3 = ax2.twinx()
+for i in range(len(best_epoch)):
+    ax1.text(best_epoch.iloc[i] + 1, test_tss.iloc[i], '{}'.format(i), c='g',
+             size=10)
 ax2 = sns.lineplot(x='epoch', y='Validation_TSS', color=color,
-                   data=all_runs[(all_runs['layers'] == layers)
-                                 & (all_runs['hidden_units'] == hidden_units)
-                                 & (all_runs['batch_size'] == batch_size)
-                                 & (all_runs['learning_rate'] == learning_rate)
-                                 & (all_runs['seed'] == seed)])
-# & (all_runs['seed'] == seed)
+                   data=run_data)
+#
 ax1.tick_params(axis='y')
 ax2 = ax1.twinx()
 color = 'tab:red'
-ax2.set_ylabel('Moving STD', fontsize=16, color=color)
-ax2.set_ylim(-0.01, 0.01)
-ax2 = sns.lineplot(x='epoch', y=pd.Series(np.gradient(all_runs[
-                                                             'moving_std'])),
+ax2.set_ylabel('Moving STD Gradient', fontsize=16, color=color)
+ax2.set_ylim(None, 0.3)
+ax2 = sns.lineplot(x='epoch', y='moving_std_w',
                    color=color,
-                   data=all_runs[(all_runs['layers'] == layers)
-                                 & (all_runs['hidden_units'] == hidden_units)
-                                 & (all_runs['batch_size'] == batch_size)
-                                 & (all_runs['learning_rate'] == learning_rate)
-                                 & (all_runs['seed'] == seed)])
+                   data=run_data)
 ax2.tick_params(axis='y', color=color)
 plt.legend(['layers:{} \nnodes:{} \nbatch_size:{} \nlearning_rate:{} \n'
            'seed:{}'
            ''.format(layers, hidden_units, batch_size, learning_rate,
-                     'all')], loc=8)
+                     'all')], loc=4)
 plt.show()
 
 
