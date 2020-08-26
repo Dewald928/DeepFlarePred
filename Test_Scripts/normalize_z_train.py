5026ad13f71a6eb0caf9fee416398e936d15e598
Dewald928 <dewald123@rocketmail.com>
2020-08-26 12:28:41 +0200

    normalization fixes
    Took 18 minutes

--- Test_Scripts/normalize_z_train.py
+++ Test_Scripts/normalize_z_train.py
@@ -12,31 +12,30 @@ df_test = pd.read_csv(filepath + 'testing.csv')
 
 # physical features
 standardscaler = StandardScaler()
-standardscaler.fit(
-    pd.concat([df_train], ignore_index=True))
+standardscaler.fit(df_train.iloc[:, 5:])
 
-phys_train_norm = pd.DataFrame(
-    standardscaler.transform(df_train),
-    index=df_train.index,
-    columns=df_train.columns)
-phys_val_norm = pd.DataFrame(
-    standardscaler.transform(df_val),
-    index=df_val.index,
-    columns=df_val.columns)
-phys_test_norm = pd.DataFrame(
-    standardscaler.transform(df_test),
-    index=df_test.index,
-    columns=df_test.columns)
+train_norm = pd.DataFrame(
+    standardscaler.transform(df_train.iloc[:, 5:]),
+    index=df_train.iloc[:, 5:].index,
+    columns=df_train.iloc[:, 5:].columns)
+val_norm = pd.DataFrame(
+    standardscaler.transform(df_val.iloc[:, 5:]),
+    index=df_val.iloc[:, 5:].index,
+    columns=df_val.iloc[:, 5:].columns)
+test_norm = pd.DataFrame(
+    standardscaler.transform(df_test.iloc[:, 5:]),
+    index=df_test.iloc[:, 5:].index,
+    columns=df_test.iloc[:, 5:].columns)
 
 
 # save as new datasets
 # make new normalized dataset
 normalized_train = pd.concat(
-    [df_train.iloc[:, :5], phys_train_norm], axis=1)
-normalized_val = pd.concat([df_val.iloc[:, :5], phys_val_norm],
+    [df_train.iloc[:, :5], train_norm], axis=1)
+normalized_val = pd.concat([df_val.iloc[:, :5], val_norm],
                            axis=1)
 normalized_test = pd.concat(
-    [df_test.iloc[:, :5], phys_test_norm], axis=1)
+    [df_test.iloc[:, :5], test_norm], axis=1)
 
 cols = df_train.columns.values
 normalized_train = normalized_train[cols]
@@ -51,5 +50,4 @@ normalized_val.to_csv(filepath_new + 'normalized_validation.csv', index=False)
 normalized_test.to_csv(filepath_new + 'normalized_testing.csv', index=False)
 
 # save scaler for inference later
-joblib.dump(standardscaler, filepath_new + '/physcial_scaler.pkl')
-joblib.dump(minmaxscaler, filepath_new + '/history_scaler.pkl')
+joblib.dump(standardscaler, filepath_new + '/scaler.pkl')
