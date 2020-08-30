5b1fc6eb609af4ac994354615c8330e40c73b9af
Dewald928 <dewald123@rocketmail.com>
2020-08-30 19:27:14 +0200

    sampler nou samples M5 ars
    Took 22 seconds

--- Test_Scripts/sampler.py
+++ Test_Scripts/sampler.py
@@ -14,6 +14,7 @@ from imblearn.over_sampling import SMOTE
 from imblearn.combine import SMOTETomek
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import LabelBinarizer
+from sklearn.datasets import make_classification
 
 sharps = ['USFLUX', 'MEANGBT', 'MEANJZH', 'MEANPOT', 'SHRGT45', 'TOTUSJH',
           'MEANGBH', 'MEANALP', 'MEANGAM', 'MEANGBZ', 'MEANJZD', 'TOTUSJZ',
@@ -25,19 +26,26 @@ history_features = ['Bdec', 'Cdec', 'Mdec', 'Xdec', 'Edec', 'logEdec', 'Bhis',
                     'Xhis1d', 'Xmax1d']  # 15
 
 filepath = '../Data/Liu/z_train/'
+drop_path = os.path.expanduser(
+    '~/Dropbox/_Meesters/figures/sampling/')
 df_train = pd.read_csv(filepath + 'normalized_training.csv')
 df_val = pd.read_csv(filepath + 'normalized_validation.csv')
 df_test = pd.read_csv(filepath + 'normalized_testing.csv')
 
 
 def plot_2d_space(X, y, label='Classes'):
-    colors = ['#1F77B4', '#FF7F0E']
-    markers = ['o', 's']
+    zorders = [0,2,1]
+    i = 0
+    colors = ['#1F77B4', '#FF7F0E', '#00ff00']
+    markers = ['o', 's', 's']
     for l, c, m in zip(np.unique(y), colors, markers):
-        plt.scatter(X[y == l, 0], X[y == l, 1], c=c, label=l, marker=m)
+        plt.scatter(X[y == l, 0], X[y == l, 1], c=c, label=l, marker=m,
+                    zorder=zorders[i])
+        i+=1
     plt.title(label)
     plt.legend(loc='upper right')
-    plt.show()
+    plt.tight_layout()
+    # plt.show()
 
 
 # Unbalanced visual
@@ -57,15 +65,13 @@ plot_2d_space(X, y, 'Imbalanced dataset (2 PCA components)')
 # rus = RandomUnderSampler()
 # X_rus, y_rus, id_rus = rus.fit_sample(X, y)
 #
-# print('Removed indexes:', id_rus)
-#
 # plot_2d_space(X_rus, y_rus, 'Random under-sampling')
 
 # cluster centroids under sampling
-cc = ClusterCentroids()
-X_cc, y_cc = cc.fit_sample(X, y)
-
-plot_2d_space(X_cc, y_cc, 'Cluster Centroids under-sampling')
+# cc = ClusterCentroids()
+# X_cc, y_cc = cc.fit_sample(X, y)
+#
+# plot_2d_space(X_cc, y_cc, 'Cluster Centroids under-sampling')
 
 # Random over sampling
 ros = RandomOverSampler()
@@ -74,30 +80,60 @@ X_ros, y_ros = ros.fit_sample(X, y)
 print(X_ros.shape[0] - X.shape[0], 'new random picked points')
 
 plot_2d_space(X_ros, y_ros, 'Random over-sampling')
+plt.show()
+# fig, ax = plt.subplots(1,1)
+# plot_resampling(X, y, ros, ax)
 
 # SMOTE over-sampling
 smote = SMOTE()
 X_sm, y_sm = smote.fit_sample(X, y)
 
 plot_2d_space(X_sm, y_sm, 'SMOTE over-sampling')
-
+plt.show()
 # over and under sampling
 smt = SMOTETomek()
 X_smt, y_smt = smt.fit_sample(X, y)
 
 plot_2d_space(X_smt, y_smt, 'SMOTE + Tomek links')
-
+plt.show()
 # inverse pca?
-X_inv = pca.inverse_transform(X_cc)
+# X_inv = pca.inverse_transform(X_cc)
+#
+# # Make new dataset
+# df_X = pd.DataFrame(data=X_inv, columns=df_train.iloc[:, 5:].columns)
+# onehot = LabelBinarizer()
+# y = onehot.fit_transform(y_cc)
+# df_y = pd.DataFrame(y, columns=['label'])
+# df_train_sampled = pd.concat([df_y, df_X], axis=1)
 
-# Make new dataset
-df_X = pd.DataFrame(data=X_inv, columns=df_train.iloc[:, 5:].columns)
-onehot = LabelBinarizer()
-y = onehot.fit_transform(y_cc)
-df_y = pd.DataFrame(y, columns=['label'])
-df_train_sampled = pd.concat([df_y, df_X], axis=1)
+'''Only use samples of ARs that flare'''
+m5_flares = df_train[df_train['label'].str.match('Positive')]
+m5_flared_NOAA = m5_flares['NOAA'].unique()
+m5_flares_data = df_train[df_train['NOAA'].isin(m5_flared_NOAA)]
+not_m5_data = df_train[~df_train['NOAA'].isin(m5_flared_NOAA)]
+not_m5_data.loc[:, 'label'] = '<M5'
+m5_flares_data.loc[m5_flares_data['label'] == 'Negative','label'] = '>M5, ' \
+                                                                    '>24h ' \
+                                                                    'before'
+m5_flares_data.loc[m5_flares_data['label'] == 'Positive','label'] = '>M5, ' \
+                                                                    '0-24h ' \
+                                                                    'before'
 
-new_path = '../Data/Sampled/'
+df = pd.concat([not_m5_data, m5_flares_data])
+
+# todo doen pca van die ook
+pca = PCA(n_components=2)
+X = pca.fit_transform(df.iloc[:, 5:])
+y = df['label']
+
+fig = plt.figure(figsize=(16,10))
+plot_2d_space(X, y, 'PCA of ARs')
+plt.savefig(drop_path + 'M5_PCA.png')
+plt.show()
+
+df_train_sampled = m5_flares_data
+
+new_path = '../Data/Liu/M5_only/'
 if not os.path.exists(new_path):
     os.makedirs(new_path)
 df_train_sampled.to_csv(new_path + 'normalized_training.csv', index=False)
