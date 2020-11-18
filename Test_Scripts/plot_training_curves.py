217cf1f98e9ba27ccee673978167d622fe2f5d87
Dewald928 <dewald123@rocketmail.com>
2020-11-18 13:07:47 +0200

    training curves plots
    Took 8 hours 2 minutes

--- Test_Scripts/plot_training_curves.py
+++ Test_Scripts/plot_training_curves.py
@@ -1,16 +1,47 @@
 import wandb
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
 api = wandb.Api()
 
 # list of runs ids
-path = 'dewald123/liu_pytorch_cnn/'
-id_dict= {0:[], 1:[]}
-id_dict[0] = ['bms1w9fy', 'd9ries1s']
-id_dict[0] = ['bms1w9fy', 'd9ries1s']
-runs_dict = {0:[],1:[]}
+dropbox = os.path.expanduser(
+    '~/Dropbox/_Meesters/figures/training_curves/')
+# hps = ['batch_size']
+path = 'dewald123/liu_pytorch_MLP/'
+id_dict = {0: [], 1: []}
+# id_dict = {0: []}
+id_dict[0] = ['nzgx1hv5', '16z66cnv', 'bugqi272']
+id_dict[1] = ['u5ewgdty', 'g7wjz699', '99nvkoax']
+runs_dict = {0: pd.DataFrame(), 1: pd.DataFrame()}
+# runs_dict = {0: pd.DataFrame()}
+colors = ['g', 'm']
+
+fig = plt.figure(figsize=(10,5))
 
 # Project is specified by <entity/project-name>
-run = api.run(f"{path}{id_dict[0][0]}")
-if run.state == "finished":
-   for i, row in run.history().iterrows():
-      print(row["_step"], row["Validation_TSS"])
+for i in range(len(id_dict)):
+    for s in range(len(id_dict[i])):
+        run = api.run(f"{path}{id_dict[i][s]}")
+        if run.state == "finished":
+            df = run.history().loc[:, ['Validation_TSS', 'Training_TSS', 'Test_TSS_curve']].dropna()
+            runs_dict[i] = pd.concat([runs_dict[i], df])
+
+    # plot curves
+    # sns.lineplot(data=runs_dict[i], x=runs_dict[i].index, y="Training_TSS", ci=68)
+    sns.lineplot(data=runs_dict[i], x=runs_dict[i].index, y="Validation_TSS", ci=68, color=colors[i])
+    # sns.lineplot(data=runs_dict[i], x=runs_dict[i].index, y="Test_TSS_curve", ci=68)
+
+# save images
+plt.xlim(xmin=0, xmax=450)
+# plt.legend(['Batch size: 65 536', 'Batch size: 1 024'])
+plt.legend(['Scaling strategy: z_train', 'Scaling strategy: z_minmax_train'], loc=4)
+plt.xlabel('Epoch')
+plt.ylabel('TSS')
+plt.title('Training curves. MLP (1_100). Validation TSS.')
+
+plt.tight_layout()
+plt.savefig(dropbox + 'TC_scaler_comp.pdf')
+plt.show()
