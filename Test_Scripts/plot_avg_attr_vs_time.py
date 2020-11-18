068053eadc7d447ba671589bfb1d633d1fc0dc7a
Dewald928 <dewald123@rocketmail.com>
2020-11-18 13:09:00 +0200

    attr plots average 24h added
    Took 1 minute

--- Test_Scripts/plot_avg_attr_vs_time.py
+++ Test_Scripts/plot_avg_attr_vs_time.py
@@ -13,7 +13,6 @@ import wandb
 
 from utils import math_stuff
 
-
 sharps = ['USFLUX', 'SAVNCPP', 'TOTPOT', 'ABSNJZH', 'SHRGT45', 'AREA_ACR',
           'R_VALUE', 'TOTUSJH', 'TOTUSJZ', 'MEANJZH', 'MEANJZD', 'MEANPOT',
           'MEANSHR', 'MEANALP', 'MEANGAM', 'MEANGBZ', 'MEANGBT',
@@ -29,25 +28,28 @@ listofuncorrfeatures = ['TOTUSJH', 'SAVNCPP', 'ABSNJZH', 'TOTPOT',
                         'MEANGAM', 'TOTFX', 'MEANJZH', 'MEANGBZ', 'TOTFZ',
                         'TOTFY', 'logEdec', 'EPSZ', 'MEANGBH', 'MEANJZD',
                         'Xhis1d', 'Xdec', 'Xhis', 'EPSX', 'EPSY', 'Bhis',
-                        'Bdec', 'Bhis1d'] # 32
-all_f = sharps+lorentz+history_features
-bad_features = ['MEANPOT', 'Mhis1d', 'Edec', 'Xhis1d', 'Bdec','Bhis',
+                        'Bdec', 'Bhis1d']  # 32
+all_f = sharps + lorentz + history_features
+bad_features = ['MEANPOT', 'Mhis1d', 'Edec', 'Xhis1d', 'Bdec', 'Bhis',
                 'Bhis1d']
 # feature_list = [x for x in all if x not in bad_features] #
 # feature_list = feature_names[5:]
 # feature_list = all_f
 attr_name_list = ["Integrated Gradients", "DeepLIFT",
-                          "Input x Gradient", "Ablation",
-                          "Shapley Value Sampling"]
+                  "Input x Gradient", "Ablation",
+                  "Shapley Value Sampling"]
 
-directory = "../saved/results/attribution/CNN/"
+directory = "../saved/results/attribution/MLP/"
 fig, axes = plt.subplots(10, 4, figsize=(15, 20), sharex=True)
 axes = axes.reshape(-1)
 df_list = []
 e = pd.DataFrame()
-df_dict = { 'IntegratedGradients': e, 'DeepLIFT': e,
+df_dict = {'IntegratedGradients': e, 'DeepLIFT': e,
            'InputxGradient': e, 'Ablation': e,
            'ShapleyValueSampling': e}
+df_24_imp = {'IntegratedGradients': e, 'DeepLIFT': e,
+             'InputxGradient': e, 'Ablation': e,
+             'ShapleyValueSampling': e}
 
 for j, attr in enumerate(attr_name_list):
     name = attr_name_list[j].replace(' ', '')
@@ -58,31 +60,47 @@ for j, attr in enumerate(attr_name_list):
             df = pd.read_csv(directory + filename)
             old_df = df_dict[name]
             df_dict[name] = pd.concat([old_df, df])
+            df_24_imp[name] = pd.concat([df_24_imp[name], df.loc[127:151, :]])
         else:
             continue
 
+# for j, attr in enumerate(attr_name_list):
+#     attr = attr.replace(' ', '')
+#     print(attr)
+#
+#     for i, feature in enumerate(all_f):
+#         print(feature)
+#
+#         ax = axes[i]
+#
+#         g = sns.lineplot(x=df_dict[attr].index, y=df_dict[attr].loc[:,feature],
+#                          data=df_dict[attr], ci=68, label=attr_name_list[j],
+#                          ax=ax)
+#         ax.axvspan(xmin=127, xmax=151, ymin=0, ymax=1, alpha=0.1, color='r')
+#         # ax.set_xscale("log")
+#         ax.set_title(feature)
+#         ax.set_ylabel('Importance')
+#         ax.set_xlabel('Sample')
+#         ax.get_legend().set_visible(False)
+#         ax.grid(True)
+#         plt.tight_layout()
+# plt.legend(title='Attribution method', loc='upper left', fontsize='x-small')
+# plt.savefig('../saved/results/attribution/CNN/atrr_avg_MLP.pdf')
+# plt.show()
+
+df_24_imp_avg = {'IntegratedGradients': e, 'DeepLIFT': e,
+                 'InputxGradient': e, 'Ablation': e,
+                 'ShapleyValueSampling': e}
+df_24_imp_std = {'IntegratedGradients': e, 'DeepLIFT': e,
+                 'InputxGradient': e, 'Ablation': e,
+                 'ShapleyValueSampling': e}
 for j, attr in enumerate(attr_name_list):
-    attr = attr.replace(' ', '')
-    print(attr)
-
-    for i, feature in enumerate(all_f):
-        print(feature)
-
-        ax = axes[i]
-
-        g = sns.lineplot(x=df_dict[attr].index, y=df_dict[attr].loc[:,feature],
-                         data=df_dict[attr], ci=68, label=attr_name_list[j],
-                         ax=ax)
-        ax.axvspan(xmin=127, xmax=151, ymin=0, ymax=1, alpha=0.1, color='r')
-        # ax.set_xscale("log")
-        ax.set_title(feature)
-        ax.set_ylabel('Importance')
-        ax.set_xlabel('Sample')
-        ax.get_legend().set_visible(False)
-        ax.grid(True)
-        plt.tight_layout()
-plt.legend(title='Attribution method', loc='upper left', fontsize='xx-small')
-plt.savefig('../saved/results/attribution/CNN/atrr_avg_CNN.pdf')
-plt.show()
+    name = attr_name_list[j].replace(' ', '')
+    df_24_imp_avg[name] = df_24_imp[name].mean().sort_values(ascending=False)
+    df_24_imp_std[name] = df_24_imp[name].std()[df_24_imp_avg[name].index.to_list()]
+    plt.bar(df_24_imp_avg[name].index.values, df_24_imp_avg[name], yerr=df_24_imp_std[name], label=name)
+    plt.xticks(rotation=90)
+    plt.legend()
+    plt.show()
 
 
